dbeb10f3f6dcf3eb827e0cdbcfa88508f1966dcb
João Pedro <jotamendes944@gmail.com>
2026-05-28 08:14:54 -0300

    CRUD completo, mas falta ajeitar

--- cassandra/update_cass.py
+++ cassandra/update_cass.py
@@ -5,77 +5,71 @@ from cassandra.cluster import Cluster
 from cassandra import ConsistencyLevel
 from cassandra.query import SimpleStatement
 
-# Configurações
-TOTAL_READS = 10000
+TOTAL_UPDATES = 10000
 TOTAL_IDS_CARREGADOS = 100000
 MAX_IN_FLIGHT = 500
 
-# Conexão
 cluster = Cluster(['127.0.0.1'])
 session = cluster.connect()
 session.set_keyspace('benchmark')
 
-inicio_ids = time.time()
-
-# Carrega apenas uma amostra dos IDs da tabela
 statement = SimpleStatement(
     f"SELECT id FROM usuarios LIMIT {TOTAL_IDS_CARREGADOS}",
     fetch_size=5000
 )
 
 rows = session.execute(statement)
+
 ids = [row.id for row in rows]
 
-fim_ids = time.time()
+print(f"IDs carregados: {len(ids)}")
 
-
-if len(ids) < TOTAL_READS:
-    raise Exception(
-        f"Apenas {len(ids)} IDs foram carregados. "
-        f"Necessário pelo menos {TOTAL_READS}."
-    )
-
-# Seleciona 10.000 IDs aleatórios
-amostra = random.sample(ids, TOTAL_READS)
+amostra = random.sample(ids, TOTAL_UPDATES)
 
 prepared = session.prepare(
-    "SELECT * FROM usuarios"
+    """
+    UPDATE usuarios
+    SET salario = ?
+    WHERE id = ?
+    """
 )
 
 prepared.consistency_level = ConsistencyLevel.ONE
 
 
-inicio_reads = time.time()
+start = time.time()
 
 futures = []
 
 for user_id in amostra:
 
+    novo_salario = round(
+        random.uniform(1500, 20000),
+        2
+    )
+
     futures.append(
         session.execute_async(
             prepared,
-            [user_id]
+            [novo_salario, user_id]
         )
     )
 
-    # Limita a quantidade de consultas simultâneas
     if len(futures) >= MAX_IN_FLIGHT:
 
         for future in futures:
-            future.result().one()
+            future.result()
 
         futures.clear()
 
-# Processa o restante
 for future in futures:
-    future.result().one()
+    future.result()
 
-fim_reads = time.time()
+end = time.time()
 
-tempo_reads = fim_reads - inicio_reads
+tempo = end - start
 
 print("\n=== RESULTADO ===")
-print(f"Leituras realizadas: {TOTAL_READS}")
-print(f"Tempo total de leitura: {tempo_reads:.2f}s")
+print(f"Tempo total: {tempo:.2f}s")
 
 cluster.shutdown()
